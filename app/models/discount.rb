70ec505830b8ac06157becf7bd9635724f6495fa
Curtis Bartell <cbartell980@gmail.com>
2020-11-09 16:38:36 -0700

    Add relationship discount belongs to merchant

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,4 +1,6 @@
 class Discount < ApplicationRecord
+  belongs_to :merchant
+
   validates :percent, presence: true, numericality: { greater_than: 0, less_than: 100 }
   validates :bulk_amount, presence: true, numericality: { greater_than: 0 }
 end
