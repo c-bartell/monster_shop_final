79f5ede955ee852b48cc5498305dc947b772cce1
Curtis Bartell <cbartell980@gmail.com>
2020-11-08 21:38:06 -0700

    Add new discount creation

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -4,6 +4,18 @@ class Merchant::DiscountsController < Merchant::BaseController
   end
 
   def new
-    
+    @discount = Discount.new
+  end
+
+  def create
+    discount = current_user.merchant.discounts.new(discount_params)
+    discount.save
+
+    redirect_to merchant_discounts_path
+  end
+
+  private
+  def discount_params
+    params.require(:discount).permit(:percent, :bulk_amount)
   end
 end
