f90e1b0eac1e56d52a99cb212143ac48143dba73
Curtis Bartell <cbartell980@gmail.com>
2020-11-08 18:10:04 -0700

    Add validation tests for discount percent and bulk_amount

---
+++ spec/models/discount_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Discount do
+  describe 'Relationships' do
+
+  end
+
+  describe 'Validations' do
+    it { should validate_presence_of :percent }
+    it { should validate_numericality_of(:percent).is_greater_than(0).is_less_than(100) }
+    it { should validate_presence_of :bulk_amount }
+    it { should validate_numericality_of(:bulk_amount).is_greater_than(0) }
+  end
+end
