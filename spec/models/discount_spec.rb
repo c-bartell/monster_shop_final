a489b76660ff146bbbfadbdafedcc090ee3b7aeb
Curtis Bartell <cbartell980@gmail.com>
2020-11-10 14:12:07 -0700

    Add tests for uniqueness of percent and bulk_amount for a merchant

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -8,8 +8,10 @@ RSpec.describe Discount do
   describe 'Validations' do
     it { should validate_presence_of :percent }
     it { should validate_numericality_of(:percent).is_greater_than(0).is_less_than(100) }
+    it { should validate_uniqueness_of(:percent).scoped_to(:merchant_id) }
     it { should validate_presence_of :bulk_amount }
     it { should validate_numericality_of(:bulk_amount).is_greater_than(0) }
+    it { should validate_uniqueness_of(:bulk_amount).scoped_to(:merchant_id) }
   end
 
   describe 'Instance Methods' do
