5543972a7ffd4ce887883daf4b76194bd143ed0c
Curtis Bartell <cbartell980@gmail.com>
2020-11-09 17:00:40 -0700

    Add #conflict?

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -3,4 +3,12 @@ class Discount < ApplicationRecord
 
   validates :percent, presence: true, numericality: { greater_than: 0, less_than: 100 }
   validates :bulk_amount, presence: true, numericality: { greater_than: 0 }
+
+  def conflict?
+    if Discount.where(percent: self.percent).or(Discount.where(bulk_amount: self.bulk_amount)) != []
+      true
+    else
+      false
+    end
+  end
 end
