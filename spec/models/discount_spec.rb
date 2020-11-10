b055de1ac796e0a29e08adcbec356a7a4e42e0ea
Curtis Bartell <cbartell980@gmail.com>
2020-11-09 17:00:00 -0700

    Add model test for #conflict?

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -12,5 +12,17 @@ RSpec.describe Discount do
     it { should validate_numericality_of(:bulk_amount).is_greater_than(0) }
   end
 
+  describe 'Instance Methods' do
+    it '#conflict?' do
+      merchant = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      discount_1 = merchant.discounts.create!(percent: 5, bulk_amount: 20)
+      new_discount_1 = merchant.discounts.new(percent: 5, bulk_amount: 20)
+      new_discount_2 = merchant.discounts.new(percent: 10, bulk_amount: 20)
+      new_discount_3 = merchant.discounts.new(percent: 10, bulk_amount: 30)
 
+      expect(new_discount_1.conflict?).to be(true)
+      expect(new_discount_2.conflict?).to be(true)
+      expect(new_discount_3.conflict?).to be(false)
+    end
+  end
 end
