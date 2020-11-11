a222617549de0db0e1ec1f845ddfff0cc3e5de25
Curtis Bartell <cbartell980@gmail.com>
2020-11-10 21:13:43 -0700

    Add tests for bulk discounts on the cart page

--- spec/features/cart/show_spec.rb
+++ spec/features/cart/show_spec.rb
@@ -9,6 +9,10 @@ RSpec.describe 'Cart Show Page' do
       @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
       @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
       @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+      @discount_1 = @megan.discounts.create!(percent: 25, bulk_amount: 20)
+      @discount_2 = @megan.discounts.create!(percent: 20, bulk_amount: 21)
+      @discount_3 = @megan.discounts.create!(percent: 50, bulk_amount: 40)
+      @discount_4 = @brian.discounts.create!(percent: 50, bulk_amount: 30)
     end
 
     describe 'I can see my cart' do
@@ -166,6 +170,48 @@ RSpec.describe 'Cart Show Page' do
         expect(current_path).to eq('/cart')
         expect(page).to_not have_content("#{@hippo.name}")
         expect(page).to have_content("Cart: 0")
+      end
+
+      it 'When there is enough of a single item in my cart for that item (only) to qualify for a bulk discount, the total an d subtotal are adjusted accordingly' do
+        @ogre.update!(inventory: 100)
+        visit item_path(@hippo)
+        click_button 'Add to Cart'
+
+        (@discount_1.bulk_amount - 1).times do
+          visit item_path(@ogre)
+          click_button 'Add to Cart'
+        end
+
+        visit '/cart'
+
+        expect(page).to have_content("Total: #{number_to_currency((@ogre.price * 19) + (@hippo.price * 1))}")
+
+        within "#item-#{@ogre.id}" do
+          expect(page).to have_content("Price: #{number_to_currency(@ogre.price)}")
+          expect(page).to have_content("Subtotal: #{number_to_currency(@ogre.price * 19)}")
+        end
+
+        within "#item-#{@hippo.id}" do
+          expect(page).to have_content("Price: #{number_to_currency(@hippo.price)}")
+          expect(page).to have_content("Subtotal: #{number_to_currency(@hippo.price * 1)}")
+        end
+
+        visit item_path(@ogre)
+        click_button 'Add to Cart'
+
+        visit '/cart'
+
+        expect(page).to have_content("Total: #{number_to_currency((@ogre.bulk_price(20) * 20) + (@hippo.price * 1))}")
+
+        within "#item-#{@ogre.id}" do
+          expect(page).to have_content("Price: #{number_to_currency(@ogre.bulk_price(20))}")
+          expect(page).to have_content("Subtotal: #{number_to_currency(@ogre.bulk_price(20) * 20)}")
+        end
+
+        within "#item-#{@hippo.id}" do
+          expect(page).to have_content("Price: #{number_to_currency(@hippo.price)}")
+          expect(page).to have_content("Subtotal: #{number_to_currency(@hippo.price * 1)}")
+        end
       end
     end
   end
